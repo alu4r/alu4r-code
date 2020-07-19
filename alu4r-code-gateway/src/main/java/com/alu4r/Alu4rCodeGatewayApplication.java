5687ca6931bee262d9d34ae3d8dd2a519313a7e7
阿璐 <763205056@qq.com>
2020-07-19 23:05:33 +0800

    添加md文件

---
+++ alu4r-code-gateway/src/main/java/com/alu4r/Alu4rCodeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.alu4r;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Alu4rCodeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Alu4rCodeGatewayApplication.class, args);
+    }
+
+}
