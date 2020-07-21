90764e7553f0713a70e5733c7ad1d4112d5ef822
阿璐 <763205056@qq.com>
2020-07-21 12:57:40 +0800

    12

--- alu4r-code-gateway/src/main/java/com/alu4r/gateway/filter/GlobalAccessTokenFilter.java
+++ alu4r-code-gateway/src/main/java/com/alu4r/gateway/filter/GlobalAccessTokenFilter.java
@@ -16,16 +16,24 @@ import java.util.stream.Collectors;
 import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
 import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.addOriginalRequestUrl;
 
+/**
+ * @author alu4r
+ */
 @Slf4j
 @Component
 public class GlobalAccessTokenFilter implements GlobalFilter, Ordered {
     public final static String X_ACCESS_TOKEN = "X-Access-Token";
 
+    /**
+     * 在该方法中，先记录了请求的开始时间，并保存在ServerWebExchange中，此处是一个“pre”类型的过滤器，然后再chain.filter的内部类中的run()方法中相当于"post"过滤器
+     * @param exchange
+     * @param chain
+     * @return
+     */
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String url = exchange.getRequest().getURI().getPath();
-
-        log.info("  access url :  "+ url);
+        log.info("access url :  "+ url);
 
         // 1. 重写StripPrefix(获取真实的URL)
         addOriginalRequestUrl(exchange, exchange.getRequest().getURI());
@@ -40,6 +48,10 @@ public class GlobalAccessTokenFilter implements GlobalFilter, Ordered {
         return chain.filter(mutableExchange);
     }
 
+    /**
+     * 用于定义优先级，值越大则优先级越低
+     * @return
+     */
     @Override
     public int getOrder() {
         return 0;
