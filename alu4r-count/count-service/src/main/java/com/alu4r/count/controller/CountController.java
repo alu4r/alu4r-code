77c06606b1c89464112876b83a7e46976c4a6ced
阿璐 <763205056@qq.com>
2020-11-19 23:08:44 +0800

    feature(*):搭建控制器

---
+++ alu4r-count/count-service/src/main/java/com/alu4r/count/controller/CountController.java
@@ -0,0 +1,16 @@
+package com.alu4r.count.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: 计数控制器
+ * @author: alu4r
+ * @date: 2020/11/19 23:01
+ */
+@RestController
+@RequestMapping("/count")
+public class CountController {
+
+
+}
---
+++ alu4r-count/query-service/src/main/java/com/alu4r/query/controller/QueryController.java
@@ -0,0 +1,15 @@
+package com.alu4r.query.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: alu4r
+ * @date: 2020/11/19 23:06
+ */
+@RestController
+@RequestMapping("/query")
+public class QueryController {
+
+}
