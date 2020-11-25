33e093e2acd8a406caa14fcaddb03840eb0c9e84
阿璐 <763205056@qq.com>
2020-11-25 21:49:15 +0800

    feature(*):spring注解相关代码

--- spring-annotation/src/main/java/com/atguigu/service/BookService.java
+++ spring-annotation/src/main/java/com/atguigu/service/BookService.java
@@ -9,24 +9,21 @@ import javax.inject.Inject;
 
 @Service
 public class BookService {
-	
-	//@Qualifier("bookDao")
-	//@Autowired(required=false)
-	//@Resource(name="bookDao2")
-	@Inject
-	private BookDao bookDao;
-	
-	public void print(){
-		System.out.println(bookDao);
-	}
 
-	@Override
-	public String toString() {
-		return "BookService [bookDao=" + bookDao + "]";
-	}
-	
-	
-	
-	
+    //@Qualifier("bookDao")
+    //@Autowired(required=false)
+    //@Resource(name="bookDao2")
+    @Inject
+    private BookDao bookDao;
+
+    public void print() {
+        System.out.println(bookDao);
+    }
+
+    @Override
+    public String toString() {
+        return "BookService [bookDao=" + bookDao + "]";
+    }
+
 
 }
