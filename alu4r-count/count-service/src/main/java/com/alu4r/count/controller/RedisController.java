0ce7921315d4d075afc44eb1cd777b4a504e130b
阿璐 <763205056@qq.com>
2020-11-29 12:46:44 +0800

    feature(service-*):redis测试

--- alu4r-count/count-service/src/main/java/com/alu4r/count/controller/RedisController.java
+++ alu4r-count/count-service/src/main/java/com/alu4r/count/controller/RedisController.java
@@ -3,20 +3,23 @@ package com.alu4r.count.controller;
 import com.alu4r.count.utils.RedisUtil;
 import com.baomidou.mybatisplus.extension.api.R;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @description: 用于测试得controller
+ * @description: redise测试
  * @author: alu4r
- * @date: 2020/11/28 15:08
+ * @date: 2020/11/28 16:58
  */
 @RestController
-@RequestMapping("test")
-public class TestController {
-    @RequestMapping("signal")
-    public R signal(){
-        return R.ok("成功发送信号！");
+@RequestMapping("redis")
+public class RedisController {
+
+    @Autowired
+    RedisUtil redisUtil;
+
+    @RequestMapping("set")
+    public R getKey(String key, String value){
+        return R.ok(redisUtil.set(key, value));
     }
 }
---
+++ alu4r-count/query-service/src/main/java/com/alu4r/query/controller/RedisController.java
@@ -0,0 +1,24 @@
+package com.alu4r.query.controller;
+
+import com.alu4r.query.utils.RedisUtil;
+import com.baomidou.mybatisplus.extension.api.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: redise测试
+ * @author: alu4r
+ * @date: 2020/11/28 16:58
+ */
+@RestController
+@RequestMapping("/redis")
+public class RedisController {
+    @Autowired
+    RedisUtil redisUtil;
+
+    @RequestMapping("get")
+    public R getKey(String key){
+        return R.ok(redisUtil.get(key));
+    }
+}
