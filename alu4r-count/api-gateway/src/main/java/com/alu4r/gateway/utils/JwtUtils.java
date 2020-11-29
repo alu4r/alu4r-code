16795097db964c51e3328d1adb8e9c25664dc0ce
阿璐 <763205056@qq.com>
2020-11-29 16:03:30 +0800

    feature(auth):添加auth

---
+++ alu4r-count/api-gateway/src/main/java/com/alu4r/gateway/utils/JwtUtils.java
@@ -0,0 +1,11 @@
+package com.alu4r.gateway.utils;
+
+
+/**
+ * @description: JWT
+ * @author: alu4r
+ * @date: 2020/11/29 15:34
+ */
+public class JwtUtils {
+
+}
---
+++ alu4r-count/auth-service/src/test/java/com/alu4r/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alu4r.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
