9129f14bfefc9379d083debb300fe4d6ce9a7ced
阿璐 <763205056@qq.com>
2020-12-20 21:30:15 +0800

    feature(*):查询、计数服务引入nacos

--- alu4r-count/count-service/count-service-biz/src/main/java/com/alu4r/count/CountServiceApplication.java
+++ alu4r-count/count-service/count-service-biz/src/main/java/com/alu4r/count/CountServiceApplication.java
@@ -2,9 +2,10 @@ package com.alu4r.count;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
+//@EnableEurekaClient
 @SpringBootApplication
 public class CountServiceApplication {
 
--- alu4r-count/query-service/query-service-biz/src/main/java/com/alu4r/query/QueryServiceApplication.java
+++ alu4r-count/query-service/query-service-biz/src/main/java/com/alu4r/query/QueryServiceApplication.java
@@ -2,12 +2,13 @@ package com.alu4r.query;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(value = {"com.alu4r"})
-@EnableEurekaClient
+@EnableDiscoveryClient
+//@EnableEurekaClient
 public class QueryServiceApplication {
 
 	public static void main(String[] args) {
