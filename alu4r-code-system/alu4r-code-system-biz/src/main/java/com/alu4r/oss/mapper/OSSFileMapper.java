894127fc3745b4b28289f4714019d9dc45720812
阿璐 <763205056@qq.com>
2020-12-21 11:00:51 +0800

    feature(*):产出不必要的代码

--- alu4r-code-system/alu4r-code-system-biz/src/main/java/com/alu4r/oss/mapper/OSSFileMapper.java
+++
@@ -1,8 +0,0 @@
-package com.alu4r.oss.mapper;
-
-import com.alu4r.oss.entity.OSSFile;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface OSSFileMapper extends BaseMapper<OSSFile> {
-
-}
--- alu4r-count/api-gateway/src/main/java/com/alu4r/gateway/GatewayApplication.java
+++ alu4r-count/api-gateway/src/main/java/com/alu4r/gateway/GatewayApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
 @EnableDiscoveryClient
 public class GatewayApplication {
 
