281a0ff2c3cecc079c38f34559532f58a893f079
阿璐 <763205056@qq.com>
2020-12-21 14:02:52 +0800

    feature(*):auth加入nacos

--- alu4r-count/auth-service/auth-service-biz/src/main/java/com/alu4r/auth/AuthServiceApplication.java
+++ alu4r-count/auth-service/auth-service-biz/src/main/java/com/alu4r/auth/AuthServiceApplication.java
@@ -2,10 +2,10 @@ package com.alu4r.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
